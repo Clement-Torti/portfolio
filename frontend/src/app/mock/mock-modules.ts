f6411a0eba72aa0b84f90a4abc4e0f262b8a561d
Clement t <ctorti@edevice.com>
2023-05-31 14:24:36 +0200

    IUT modules done

--- frontend/src/app/mock/mock-modules.ts
+++ frontend/src/app/mock/mock-modules.ts
@@ -5,322 +5,324 @@ import { ModuleCategory } from '../models/moduleCategory';
 export const MOCKED_MODULES: Module[] = [
     // IUT
     new Module(
-        'Algorithms', 
-        SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        'Algorithms',
+        SchoolCategory.IUT,
+        '4 months',
+        'This module explores fundamental algorithms and data structures, covering topics such as sorting, searching, graph algorithms, and dynamic programming.',
+        [],
         ModuleCategory.PROGRAMMING
-    ),
+        ),
+
 
     new Module(
         'Arithmetic', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module focuses on arithmetic operations, number systems, algorithms, and mathematical problem-solving techniques.' , 
+        [],
         ModuleCategory.MATH
     ),
 
     new Module(
         'Documentation & Interface (HTML & CSS)', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module introduces the fundamentals of HTML and CSS, teaching students how to create well-structured and visually appealing web pages.' , 
+        [],
         ModuleCategory.WEB
     ),
     
     new Module(
         'Software Economics', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module covers the economic aspects of software development, including cost estimation, project planning, budgeting, and software pricing strategies.' , 
+        [],
         ModuleCategory.MANAGEMENT
     ),
     new Module(
         'Information Systems', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module explores the design, implementation, and management of information systems, covering topics such as database systems, data modeling, and system integration.' , 
+        [],
         ModuleCategory.NETWORKING
     ),
 
     new Module(
         'Linear Algebra', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module covers the fundamental concepts and techniques of linear algebra, including vector spaces, matrices, systems of linear equations, and linear transformations.' , 
+        [],
         ModuleCategory.MATH
     ),
     
     new Module(
         'Databases', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module introduces the principles of database design and management, including data modeling, relational databases, SQL querying, and database administration.' , 
+        [],
         ModuleCategory.DATABASE
     ),
     
     new Module(
         'Business Management', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module provides an overview of business management principles and practices, covering topics such as organizational behavior, strategic planning, marketing, and financial management.' , 
+        [],
         ModuleCategory.MANAGEMENT
     ),
     new Module(
         'Data Structures', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module focuses on the implementation and analysis of fundamental data structures, such as arrays, linked lists, stacks, queues, trees, and graphs, along with algorithms for efficient data manipulation and retrieval.' , 
+        [],
         ModuleCategory.ALGORITHM
     ),
 
     new Module(
         'Architecture', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module explores computer architecture principles, including the organization and design of computer systems, instruction set architecture, memory hierarchy, and input/output systems.' , 
+        [],
         ModuleCategory.SYSTEM
     ),
     
     new Module(
         'Budget Management', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module focuses on budget planning, financial analysis, cost control, and resource allocation techniques to effectively manage budgets in organizational settings.' , 
+        [],
         ModuleCategory.MANAGEMENT
     ),
     
     new Module(
         'Graphs & Automata', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module covers graph theory and automata theory, including graph algorithms, connectivity, shortest paths, regular languages, finite automata, and formal language theory.' , 
+        [],
         ModuleCategory.ALGORITHM
     ),
+
     new Module(
         'Object-oriented Programming', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module introduces the principles and concepts of object-oriented programming, including encapsulation, inheritance, polymorphism, and design patterns, using a programming language such as Java or C++.' , 
+        [],
         ModuleCategory.PROGRAMMING
     ),
 
     new Module(
         'Analysis & Numerical Methods', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module focuses on mathematical analysis techniques, including limits, derivatives, integrals, numerical methods, and approximation algorithms, to solve mathematical problems and analyze real-world phenomena.' , 
+        [],
         ModuleCategory.MATH
     ),
     
     new Module(
         'Object-Oriented Programming and Human-Computer Interaction', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module combines the principles of object-oriented programming with the fundamentals of human-computer interaction, covering topics such as user interface design, usability testing, event-driven programming, and graphical user interface development.' , 
+        [],
         ModuleCategory.PROGRAMMING
     ),
     
     new Module(
         'Labor Law', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module provides an overview of labor law principles and regulations, focusing on topics such as employment contracts, worker rights, labor disputes, occupational health and safety, and legal considerations in the workplace.' , 
+        [],
         ModuleCategory.SOFT_SKILL
     ),
 
     new Module(
         'Project Management', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module covers the principles and practices of project management, including project planning, scheduling, resource management, risk assessment, team coordination, and project evaluation techniques.' , 
+        [],
         ModuleCategory.MANAGEMENT
     ),
 
     new Module(
         'Advanced Algorithms', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module explores advanced algorithm design and analysis techniques, including divide and conquer, greedy algorithms, dynamic programming, network flows, NP-completeness, and approximation algorithms.' , 
+        [],
         ModuleCategory.ALGORITHM
     ),
 
     new Module(
         'Advanced Databases', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module delves deeper into database concepts, focusing on advanced topics such as advanced SQL queries, database optimization, indexing techniques, database security, distributed databases, and data warehousing.' , 
+        [],
         ModuleCategory.DATABASE
     ),
 
     new Module(
         'Probability and Statistics', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module introduces the fundamental principles of probability theory and statistical analysis, covering topics such as probability distributions, hypothesis testing, regression analysis, probability models, and statistical inference.' , 
+        [],
         ModuleCategory.MATH
     ),
     
     new Module(
         'Network Services', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module explores network services and protocols, covering topics such as network architecture, routing, switching, DNS, DHCP, FTP, email services, web services, network security, and network performance optimization.' , 
+        [],
         ModuleCategory.NETWORKING
     ),
 
     new Module(
         'Industrial Strategies', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module focuses on understanding and analyzing industrial strategies in the context of business environments, including strategic planning, competitive analysis, market research, product positioning, and business development strategies.' , 
+        [],
         ModuleCategory.SOFT_SKILL
     ),
 
     new Module(
         'ICT Law', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module provides an overview of legal and ethical considerations in the field of information and communication technology (ICT), including intellectual property rights, privacy laws, data protection regulations, cybersecurity, and ethical practices in ICT.' , 
+        [],
         ModuleCategory.SOFT_SKILL
     ),
     
     new Module(
         'Project Management 2', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module builds upon the principles learned in the initial project management module, delving deeper into advanced project management techniques, including project risk management, agile methodologies, project quality assurance, and project monitoring and control.' , 
+        [],
         ModuleCategory.MANAGEMENT
     ),
     
     new Module(
         'Mathematical Modeling', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module focuses on mathematical modeling techniques to represent and solve real-world problems, covering topics such as differential equations, optimization, discrete modeling, statistical modeling, and computational simulation methods.' , 
+        [],
         ModuleCategory.MATH
     ),
 
     new Module(
         'Advanced Object-Oriented Programming (JavaFX)', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module focuses on advanced concepts of object-oriented programming using JavaFX, covering topics such as graphical user interface (GUI) development, event handling, multimedia integration, animations, and advanced application design patterns.' , 
+        [],
         ModuleCategory.PROGRAMMING
     ),
 
     new Module(
         'Operating System Programming', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module explores the principles of operating system design and implementation, covering topics such as process management, memory management, file systems, device drivers, inter-process communication, and system-level programming using languages like C or C++.' , 
+        [],
         ModuleCategory.SYSTEM
     ),
     
     new Module(
         'Server-Side Web Programming', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module focuses on server-side web development techniques, covering topics such as web application architecture, server-side scripting languages (PHP), database integration, security considerations, and web service APIs.' , 
+        [],
         ModuleCategory.WEB
     ),
     
     new Module(
         'Entrepreneurship Workshop', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module provides practical knowledge and skills necessary for entrepreneurship, including idea generation, business planning, marketing strategies, financial management, legal aspects, and startup funding.' , 
+        [],
         ModuleCategory.SOFT_SKILL
     ),
     new Module(
         'C++', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module introduces the C++ programming language, covering topics such as syntax and semantics, object-oriented programming concepts, memory management, file handling, templates, and advanced features like exception handling and multithreading.' , 
+        [],
         ModuleCategory.PROGRAMMING
     ),
 
     new Module(
         'Linear Algebra / Analysis of Algorithm Complexity', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module covers two key areas: linear algebra and the analysis of algorithm complexity. Topics include matrix operations, vector spaces, eigenvalues and eigenvectors, algorithmic analysis, time complexity, space complexity, and Big O notation.' , 
+        [],
         ModuleCategory.MATH
     ),
     
     new Module(
         'Interpreted Object-Oriented Programming', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module explores interpreted object-oriented programming languages, such as Python or Ruby, focusing on language features, dynamic typing, metaprogramming, script development, and the integration of object-oriented principles in interpreted environments.' , 
+        [],
         ModuleCategory.PROGRAMMING
     ),
     
     new Module(
         'Mobile Programming', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module focuses on mobile application development, covering topics such as mobile platforms (Android or iOS), user interface design, application lifecycle, data storage, network communication, and mobile app deployment.' , 
+        [],
         ModuleCategory.PROGRAMMING
     ),
 
     new Module(
         'Virtual Reality', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '4 months', 
+        'This module explores the principles and techniques of virtual reality (VR) development, covering topics such as VR hardware, 3D modeling and rendering, user interaction, immersive experiences, VR programming frameworks, and VR application design considerations.' , 
+        [],
         ModuleCategory.PROGRAMMING
     ),
 
     new Module(
         'English', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '5 years', 
+        'This module aims to enhance English language skills, including reading, writing, listening, and speaking. Topics may include vocabulary expansion, grammar review, comprehension exercises, oral presentations, and communication strategies.' , 
+        [],
         ModuleCategory.SOFT_SKILL
     ),
 
     new Module(
         'Expression & Professional Presentation', 
         SchoolCategory.IUT, 
-        '', 
-        '' , 
-        [''],
+        '2 years', 
+        'This module focuses on developing effective communication and presentation skills for professional settings. Topics include public speaking, effective writing, persuasive techniques, visual aids, body language, and professional etiquette.' , 
+        [],
         ModuleCategory.SOFT_SKILL
     ),
 
@@ -328,7 +330,6 @@ export const MOCKED_MODULES: Module[] = [
 
         
     // ENSEEIHT
-
     new Module(
         'Computer architecture', 
         SchoolCategory.ENSEEIHT, 
@@ -431,7 +432,7 @@ export const MOCKED_MODULES: Module[] = [
         '1 semester', 
         'The objectives of this course are to learn how to build a graphical user interface, gain practical experience with object-oriented concepts, and understand event-driven programming and its implementation in Java. The course plan includes studying state-transition diagrams, principles of user interface, constructing the presentation (view), event handling, and concluding remarks. Using JavaFX/Swing.' , 
         ['Xavier Crégut'],
-        ModuleCategory.DATABASE
+        ModuleCategory.PROGRAMMING
     ),
 
     new Module(
@@ -527,7 +528,7 @@ export const MOCKED_MODULES: Module[] = [
         SchoolCategory.ENSEEIHT, 
         '', 
         '1 semester' , 
-        [''],
+        [],
         ModuleCategory.NETWORKING
     ),
     
@@ -579,8 +580,8 @@ export const MOCKED_MODULES: Module[] = [
     new Module(
         'Statistics', 
         SchoolCategory.ENSEEIHT, 
-        'Learn how to collect, analyze, and interprete numerical data to uncover patterns, relationships, and trends. It provides tools and techniques for making informed decisions and drawing meaningful conclusions from data.', 
-        '1 semester' , 
+        '1 semester', 
+        'Learn how to collect, analyze, and interprete numerical data to uncover patterns, relationships, and trends. It provides tools and techniques for making informed decisions and drawing meaningful conclusions from data.' , 
         ['Vincent Charvillat'],
         ModuleCategory.MATH
     ),
@@ -641,7 +642,7 @@ export const MOCKED_MODULES: Module[] = [
     
     new Module(
         'Database', 
-        SchoolCategory.UCM, 
+        SchoolCategory.ENSEEIHT, 
         '1 semester', 
         `introduce the basic concepts necessary for the rigorous design of databases. Skills acquired: good grasp of the basic concepts of database design.
         Sequence 1: Conceptual modelling and relational databases
